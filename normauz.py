57bde3013efa8b4ad428eb2072a2d7d3f123f423
UlugbekSalaev <ulugbek0302@gmail.com>
2022-07-01 17:45:41 +0500

    Fourth commit

--- normauz.py
+++ normauz.py
@@ -14,6 +14,8 @@ mydb = mysql.connector.connect(
 )
 
 cursor = mydb.cursor()
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 
 def write_in_file(self, text):
@@ -25,14 +27,14 @@ def write_in_file(self, text):
 def get_text(url, category):
     print("Start: " + url)
     try:
-        r = requests.get(url).text
+        r = requests.get(url, headers=headers).text
         soup = BeautifulSoup(r, 'html.parser')
         if "Not Found" in r:
             print(url + " : not found")
             return
 
         # category = soup.find(class_='itemCat').text
-        title = soup.find(class_='post-title').text
+        title = soup.find(class_='pos_0').text
         date = soup.find(class_='date').text
         text = ''
 
@@ -40,9 +42,11 @@ def get_text(url, category):
         # for x in table:
         #     text += x.find('p').text + '\n\n'
 
-        parag = soup.find_all('p')
+        parag = soup.find(class_='fullText').find_all('p')
         for x in parag:
             if 'Foto:' in x.text:
+                continue
+            if x.text.strip() == '':
                 continue
             text += x.text + '\n\n'
 
@@ -56,26 +60,25 @@ def get_text(url, category):
 
 
 def get_urls(url):
-    '''Returns data specifically from kun.uz/uz.'''
-    page = requests.get(url).text
+    '''Returns data specifically from norma.uz/uz.'''
+    page = requests.get(url, headers=headers).text
     soup = BeautifulSoup(page, "lxml")
     if "Sahifa topilmadi" in page:
         print(url + " : not found")
         return
-    i = 0
-    for p in soup.find_all(class_="post-body"):
-        i += 1
-        if i < 4:
-            continue
-        url1 = p.find('a')['href']
-        url1 = "https://m.kun.uz" + url1
-        # caturl = p.find(class_="data").find('a')['href']
-        category = p.find(class_="data").find('a').text.strip()
-        get_text(url1, category)
+
+    for p in soup.find(class_='categoryItems').find_all(class_="item"):
+        try:
+            url1 = p.find('a')['href']
+            url1 = "https://www.norma.uz" + url1
+            # caturl = p.find(class_="data").find('a')['href']
+            category = p.find(class_="serviceInfo").find('a').text.strip()
+            get_text(url1, category)
+        except:
+            print("Bir narsa xatolik!")
     mydb.commit()
 
-
-for i in range(4498, 11582):
+for i in range(216, 300):
     print("Page:" + str(i))
-    get_urls("https://m.kun.uz/uz?q=%2Fuz&page=" + str(i))
+    get_urls("https://www.norma.uz/oz/?page=" + str(i))
     print("--- %s minutes ---" % ((time.time() - start_time) / 60))
